f95e834c7239fbb5fac69c6441bb45e30b9a0715
Muhammad Sunaam <msunaam2002@gmail.com>
2024-04-07 16:32:48 +0500

    update webhook

--- flask_app.py
+++ flask_app.py
@@ -1,3 +1,4 @@
+import json
 from flask import Flask, request
 import git
 
@@ -9,7 +10,13 @@ def hello_world():
 
 @app.route('/update_server', methods=['POST'])
 def webhook():
+    
     if request.method == 'POST':
+        event = request.headers.get('X-GitHub-Event')
+        if event == "ping":
+            return json.dumps({'msg': 'Hi!'}), 200
+        if event != "push":
+            return json.dumps({'msg': "Wrong event type"}), 400
         repo = git.Repo('path/to/git_repo')
         origin = repo.remotes.origin
         origin.pull()
